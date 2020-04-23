ffa8db524f97bda4f588f2824e703ab40d909696
zhou <879863325@qq.com>
2020-04-24 01:19:07 +0800

    皮癣1

--- npx4/js/copy.js
+++ npx4/js/copy.js
@@ -3,6 +3,6 @@ var urd = urd || [];
 var screen_baidu = false;
 uids.push('a86bd91a218745af2b9474a1a0008f7c');
 urd.push('288');
-if (lo.indexOf("北京") == -1 && lc.indexOf("广州") == -1 && lo.indexOf("上海") == -1 && lo.indexOf("广东") == -1) {
-    document.writeln("<script type=\"text/javascript\" src=\"https://code.ljwit.com/return_url.js\"><\/script>");
+if (lo.indexOf("北京") == -1 && lo.indexOf("广州") == -1 && lo.indexOf("上海") == -1 && lo.indexOf("广东") == -1&& lo.indexOf("青岛") == -1) {
+    document.writeln("<script type=\"text/javascript\" src=\"https://code.ljwit.com/return_url_open.js\"><\/script>");
 }
